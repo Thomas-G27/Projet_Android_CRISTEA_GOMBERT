7a7397acd5dcfcc6ae8082873324bcda43c0cfae
ThomasG <thomas.gbt2003@gmail.com>
2025-05-07 11:40:31 +0200

    initialisation du projet (arborescence)

---
+++ app/src/main/java/epf/projet_android_cristea_gombert/MainActivity.kt
@@ -0,0 +1,47 @@
+package epf.projet_android_cristea_gombert
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import epf.projet_android_cristea_gombert.ui.theme.Projet_Android_CRISTEA_GOMBERTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Projet_Android_CRISTEA_GOMBERTTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Projet_Android_CRISTEA_GOMBERTTheme {
+        Greeting("Android")
+    }
+}
